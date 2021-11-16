8d00efcbbf033a8d824ad8f27fbafa384cd3babb
Maycon Rodrigo <mayconrodrigo123@hotmail.com>
2021-11-16 15:24:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 8080;
+const port = 80;
 
 //importar o model pergunta (criação da tabela)
 const pergunta = require('./database/Pergunta');
